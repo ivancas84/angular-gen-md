11ecb66de077058008002b4764147215bf40a581
ivancas84 <ivancas84@gmail.com>
2020-07-24 09:39:31 -0300

    Corregir errores en html generado de Admin

--- src/component/admin/AdminHtml.php
+++ src/component/admin/AdminHtml.php
@@ -12,24 +12,18 @@ class Gen_AdminHtml extends GenerateFileEntity {
   }
 
   public function generateCode() {
-    $this->string .= "<mat-card>
-    <mat-card-header>
-      <mat-card-title>" . $this->getEntity()->getName("Xx Yy") . "</mat-card-title>
-    </mat-card-header>
-  </mat-card>
-  <mat-card-content>
-  
-  <form class=\"container\" [formGroup]=\"adminForm\" (ngSubmit)=\"onSubmit()\" novalidate  autocomplete=\"off\">
-    <app-" . $this->getEntity()->getName("xx-yy") . "-fieldset [form]=\"adminForm\" [data\$]=\"data\$\"></app-" . $this->getEntity()->getName("xx-yy") . "-fieldset>
-    <button mat-raised-button [disabled]=\"adminForm.pending && isSubmitted\" type=\"submit\" color=\"primary\">Aceptar</button>&nbsp;
-    <button mat-mini-fab color=\"accent\" type=\"button\"  (click)=\"back()\"><mat-icon>keyboard_backspace</mat-icon></button>
-    <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"reset()\"><mat-icon>clear_all</mat-icon></button>
-    <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"clear()\"><mat-icon>add</mat-icon></button>
-    <button mat-mini-fab color=\"warn\" type=\"button\" [disabled]=\"isDeletable\" (click)=\"delete()\"><mat-icon>delete</mat-icon></button>
+    $this->string .= "<form [formGroup]=\"adminForm\" (ngSubmit)=\"onSubmit()\" novalidate autocomplete=\"off\">
+  <app-" . $this->getEntity()->getName("xx-yy") . "-fieldset [form]=\"adminForm\" [data\$]=\"data\$\"></app-" . $this->getEntity()->getName("xx-yy") . "-fieldset>
 
-    <!--p>Debug Form value: {{ adminForm.value | json }}</p>
-    <p>Debug Form status: {{ adminForm.status | json }}</p-->
-  </form>
+  <button mat-raised-button [disabled]=\"adminForm.pending && isSubmitted\" type=\"submit\" color=\"primary\">Aceptar</button>&nbsp;
+  <button mat-mini-fab color=\"accent\" type=\"button\"  (click)=\"back()\"><mat-icon>keyboard_backspace</mat-icon></button>
+  <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"reset()\"><mat-icon>clear_all</mat-icon></button>
+  <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"clear()\"><mat-icon>add</mat-icon></button>
+  <button mat-mini-fab color=\"warn\" type=\"button\" [disabled]=\"isDeletable\" (click)=\"delete()\"><mat-icon>delete</mat-icon></button>
+
+  <!--p>Debug Form value: {{ adminForm.value | json }}</p>
+  <p>Debug Form status: {{ adminForm.status | json }}</p-->
+</form>
 ";
 
   }
