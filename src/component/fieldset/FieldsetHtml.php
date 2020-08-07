767667db630677af432da91fa7c7e5fe8f3f80fb
ivancas84 <ivancas84@gmail.com>
2020-08-07 10:40:00 -0300

    Emprolijar codigo

--- src/component/fieldset/FieldsetHtml.php
+++ src/component/fieldset/FieldsetHtml.php
@@ -231,13 +231,13 @@ class FieldsetHtml extends GenerateFileEntity {
 
   protected function select(Field $field) {
     $this->newRow();
-    $this->string .= "        <app-input-select fxFlex=\"auto\" [field]=\"{$field->getName('xxYy')}\" [entityName]=\"'{$field->getEntityRef()->getName()}'\" [title]=\"'{$field->getEntityRef()->getName("Xx Yy")}'\"></app-input-select>
+    $this->string .= "        <app-input-select fxFlex=\"auto\" [field]=\"{$field->getName('xxYy')}\" [entityName]=\"'{$field->getEntityRef()->getName()}'\" [title]=\"'{$field->getName("Xx Yy")}'\"></app-input-select>
 ";
   }
 
   protected function autocomplete(Field $field) {
     $this->newRow();
-    $this->string .= "        <app-input-autocomplete fxFlex=\"auto\" [field]=\"" . $field->getName("xxYy") . "\" [entityName]=\"'" . $field->getEntityRef()->getName() . "'\" [title]=\"'{$field->getEntityRef()->getName("Xx Yy")}'\"></app-input-autocomplete>
+    $this->string .= "        <app-input-autocomplete fxFlex=\"auto\" [field]=\"" . $field->getName("xxYy") . "\" [entityName]=\"'" . $field->getEntityRef()->getName() . "'\" [title]=\"'{$field->getName("Xx Yy")}'\"></app-input-autocomplete>
 ";
   }
 
