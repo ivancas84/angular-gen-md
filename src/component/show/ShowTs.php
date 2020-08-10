2054ca1e1f6d13a0eeabdd63c7a434afa6f213c7
ivancas84 <ivancas84@gmail.com>
2020-08-10 09:56:11 -0300

    Incorporar generacion de componente show

---
+++ src/component/show/ShowTs.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("GenerateFileEntity.php");
+
+class Gen_ShowTs extends GenerateFileEntity {
+
+  public function __construct(Entity $entity) {
+    $dir = $_SERVER["DOCUMENT_ROOT"]."/".PATH_GEN."/" . "tmp/component/show/" . $entity->getName("xx-yy") . "-show/";
+    $file = $entity->getName("xx-yy") . "-show.component.ts";
+    parent::__construct($dir, $file, $entity);
+  }
+
+  protected function start(){
+    $this->string .= "import { Component } from '@angular/core';
+import { ShowComponent } from '@component/show/show.component';
+
+@Component({
+  selector: 'app-" . $this->entity->getName("xx-yy") . "-show',
+  templateUrl: './" . $this->entity->getName("xx-yy") . "-show.component.html',
+})
+export class " . $this->entity->getName("XxYy") . "ShowComponent extends ShowComponent {
+
+  readonly entityName: string = \"" . $this->entity->getName() . "\";
+
+}
+
+";
+  }
+
+  protected function generateCode() { //@override
+    $this->start();
+  }
+
+}
