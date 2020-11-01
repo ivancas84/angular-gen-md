5674f039605db1ba210242e2eb881dad57120059
ivancas84 <ivancas84@gmail.com>
2020-11-01 19:17:21 -0300

    Incorporar nuevos parametros de inicializacion

--- src/component/admin/AdminHtml.php
+++ src/component/admin/AdminHtml.php
@@ -12,17 +12,22 @@ class Gen_AdminHtml extends GenerateFileEntity {
   }
 
   public function generateCode() {
-    $this->string .= "<form [formGroup]=\"adminForm\" (ngSubmit)=\"onSubmit()\" novalidate autocomplete=\"off\">
-  <app-" . $this->getEntity()->getName("xx-yy") . "-fieldset [form]=\"adminForm\" [data\$]=\"data\$\"></app-" . $this->getEntity()->getName("xx-yy") . "-fieldset>
-
-  <button mat-raised-button [disabled]=\"adminForm.pending || isSubmitted\" type=\"submit\" color=\"primary\">Aceptar</button>&nbsp;
-  <button mat-mini-fab color=\"accent\" type=\"button\"  (click)=\"back()\"><mat-icon>keyboard_backspace</mat-icon></button>
-  <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"reset()\"><mat-icon>clear_all</mat-icon></button>
-  <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"clear()\"><mat-icon>add</mat-icon></button>
-  <button mat-mini-fab color=\"warn\" type=\"button\" [disabled]=\"isDeletable\" (click)=\"delete()\"><mat-icon>delete</mat-icon></button>
-
-  <!--p>Debug Form value: {{ adminForm.value | json }}</p>
-  <p>Debug Form status: {{ adminForm.status | json }}</p-->
+    $this->string .= "<ng-template #loading>
+    <mat-progress-bar mode=\"indeterminate\"></mat-progress-bar>
+  </ng-template>
+  
+  <form *ngIf=\"(loadParams$ | async)\" [formGroup]=\"adminForm\" (ngSubmit)=\"onSubmit()\" novalidate autocomplete=\"off\">
+    <div *ngIf=\"(loadDisplay$ | async) && load; else loading\">  
+      <app-" . $this->getEntity()->getName("xx-yy") . "-fieldset [form]=\"adminForm\" [data\$]=\"data\$\"></app-" . $this->getEntity()->getName("xx-yy") . "-fieldset>
+    
+      <button mat-raised-button [disabled]=\"adminForm.pending || isSubmitted\" type=\"submit\" color=\"primary\">Aceptar</button>&nbsp;
+      <button mat-mini-fab color=\"accent\" type=\"button\"  (click)=\"back()\"><mat-icon>keyboard_backspace</mat-icon></button>
+      <button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"reset()\"><mat-icon>clear_all</mat-icon></button>
+      <!--button mat-mini-fab color=\"accent\" type=\"button\" (click)=\"clear()\"><mat-icon>add</mat-icon></button-->
+      <!--button mat-mini-fab color=\"warn\" type=\"button\" [disabled]=\"isDeletable\" (click)=\"delete()\"><mat-icon>delete</mat-icon></button-->
+      <!--p>Debug Form value: {{ adminForm.value | json }}</p>
+      <p>Debug Form status: {{ adminForm.status | json }}</p-->
+    </div>
 </form>
 ";
 
