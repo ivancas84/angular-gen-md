b94093da4fc7601643f18fe4b8b8f2abcdc7e1f3
ivancas84 <ivancas84@gmail.com>
2020-11-18 12:33:50 -0300

    Reemplazar uso de clase estatica StructTools por instancia a partir de container

--- src/component/_initOptions/_DeclareOptions.php
+++ src/component/_initOptions/_DeclareOptions.php
@@ -1,16 +1,17 @@
 <?php
 
 require_once("GenerateEntity.php");
-require_once("class/controller/StructTools.php");
+require_once("class/Container.php");
 
 
 class Gen_declareOptions extends GenerateEntity {
  
 
   public function generate($entities = NULL) {
+    $container = new Container();
     switch($entities){
-      case "unique_multiple": $this->entities = StructTools::getEntityRefBySubtypeSelectUniqueMultiple($this->entity); break;
-      default: $this->entities = StructTools::getEntityRefBySubtypeSelect($this->entity); break;
+      case "unique_multiple": $this->entities = $container->getController("struct_tools")->getEntityRefBySubtypeSelectUniqueMultiple($this->entity); break;
+      default: $this->entities = $container->getController("struct_tools")->getEntityRefBySubtypeSelect($this->entity); break;
     }
 
     if(!count($this->entities)) return;
